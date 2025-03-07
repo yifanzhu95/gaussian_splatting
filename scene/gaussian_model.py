5c258960eeeda1a4f5e416f850d076f67f8fca0d
grablab <yifanzhu95@gmail.com>
2025-03-07 15:24:24 -0500

    Use custom distCUDA2 to avoid installation of simple_knn

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -18,11 +18,19 @@ import os
 from ..utils.system_utils import mkdir_p
 from plyfile import PlyData, PlyElement
 from ..utils.sh_utils import RGB2SH
-from simple_knn._C import distCUDA2
+# from simple_knn._C import distCUDA2
 from ..utils.graphics_utils import BasicPointCloud
 from ..utils.general_utils import strip_symmetric, build_scaling_rotation
 from icecream import ic
+from scipy.spatial import KDTree
+import torch
 
+def distCUDA2(points):
+    points_np = points.detach().cpu().float().numpy()
+    dists, inds = KDTree(points_np).query(points_np, k=4)
+    meanDists = (dists[:, 1:] ** 2).mean(1)
+
+    return torch.tensor(meanDists, dtype=points.dtype, device=points.device)
 class MultiGaussianModel:
     """ Work with SAP, where the positions of the splats are not optimized."""
     def setup_functions(self):
